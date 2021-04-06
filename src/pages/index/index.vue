e3e7e0a8a01bbc86ac6ec4743745856a846d1811
yucw@itsmycar.cn <123456>
2021-04-06 19:46:21 +0800

    上传脚手架：uni-app + uViewUI

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,49 +1,28 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+  <view>
+    <u-button type="success">
+      <text>{{title}} 框架真好</text>
+      <u-icon name="thumb-up-fill" color="#ffffff" size="28"></u-icon>
+    </u-button>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+export default {
+  data() {
+    return {
+      title: 'uView ui'
+    }
+  },
+  onLoad() {
 
-		},
-		methods: {
+  },
+  methods: {
 
-		}
-	}
+  }
+}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+<style lang="scss">
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
